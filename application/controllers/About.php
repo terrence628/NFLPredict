428b8341ea5caf71edb8acce41cf7a3c09b1e708
terrence628 <terrence_lam@hotmail.com>
2015-10-01 14:53:52 -0700

    added lab2

---
+++ application/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About extends Application {
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+            //$this->load->view('about');
+            $this->data['pagebody'] = 'about';
+            $this->render();
+	}
+}
